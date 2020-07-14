2fba8c4cd6f5f13bf54e6bf06d3020c3170e1bfb
AntMan <xanthonyalvarezx@gmail.com>
2020-07-14 11:20:43 -0400

    initial commit

--- assets/scripts/classes.js
+++ assets/scripts/classes.js
@@ -1,4 +1,3 @@
-
 class Traveler {
     constructor(name, food = 1, isHealthy = true) {
         this.name = name
@@ -7,17 +6,66 @@ class Traveler {
     }
     hunt() {
         this.food += 2
- 
+
     }
     eat() {
-        
+
         if (this.food > 0) {
             this.food -= 1
         } else {
             this.isHealthy = false
-            }
-        
+        }
+
     }
+}
+
+//---------- Doctor ------------//
+class Doctor extends Traveler {
+    constructor(name, food = 1, isHealthy = true) {
+        super()
+        this.name = name
+        this.food = food
+        this.isHealthy = isHealthy
+
+    }
+
+    heal(Traveler) {
+        Traveler.isHealthy = true
+    }
+}
+
+//---------- Hunter ------------//
+
+class Hunter extends Traveler {
+    constructor(name, food = 2, isHealthy = true) {
+        super()
+        this.name = name
+        this.food = food
+        this.isHealthy = isHealthy
+    }
+    hunt() {
+        this.food += 5
+
+    }
+    eat() {
+
+        if (this.food > 0) {
+            this.food -= 2
+        }
+        if (this.food < 2) {
+            this.food = 0
+            this.isHealthy = false
+        }
+
+    }
+    giveFood(traveler, numberOfFoodUnits) {
+        if (this.food > numberOfFoodUnits) {
+            this.food -= numberOfFoodUnits
+            traveler.food += numberOfFoodUnits
+
+        }
+    }
+
 }
 ////////////  wagon //////////////
 
@@ -34,9 +82,10 @@ class Wagon {
 
     }
 
-    join(traveler) {
+    join(Traveler) {
         if (this.getAvailableSeatCount() > 0) {
-            this.passengers.push(traveler)
+            this.passengers.push(Traveler)
+
         }
 
     }
@@ -54,13 +103,64 @@ class Wagon {
 
     }
 
-    totalFood(){
+    totalFood() {
         let foodSum = this.passengers.reduce((sum, traveler) => {
             let result = sum + traveler.food
             console.log(result)
             return result
-        },0)
+        }, 0)
         return foodSum
     }
 }
 
+
+
+function newTraveler() {
+    newPerson = document.createElement('div')
+    newPerson.class = 'person'
+    let nextTraveler = new Traveler(travelerName.value)
+    let personImage = document.createElement('img')
+    personImage.src = 'assets/farmer.png'
+    let eatFood = document.createElement('button')
+    let huntForFood = document.createElement('button')
+    eatFood.class = 'eat'
+    eatFood.innerText = 'Eat'
+    huntForFood.class = 'hunt'
+    huntForFood.innerText = 'Hunt'
+    newPerson.append(eatFood, huntForFood, personImage, JSON.stringify(nextTraveler))
+    main.append(newPerson)
+    currentTravelers.append(newPerson, )
+    ///---------------------------///
+    eatFood.addEventListener('click', function () {
+        console.log('clicked')
+        nextTraveler.eat()
+    })
+}
+
+addTraveler.addEventListener('click', function () {
+    newTraveler()
+
+
+})
+
+function newWagon() {
+    nextWagon = document.createElement('div')
+    let newWagon = new Wagon(newWagonCapacity.value)
+
+    let quarentineWagon = document.createElement('button')
+    quarentineWagon.class = 'createWagon'
+    quarentineWagon.innerText = ' Quarentine?'
+
+    wagonImg = document.createElement('img')
+    wagonImg.src = 'assets/oldwagon.png'
+    main.append(nextWagon)
+    nextWagon.append(JSON.stringify(newWagon), quarentineWagon, wagonImg)
+
+}
+
+createWagon.addEventListener('click', function () {
+    newWagon()
+
+
+
+})
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,40 +1,56 @@
-let wagon = new Wagon(2)
-// Create three travelers
-let henrietta = new Traveler('Henrietta')
-let juan = new Traveler('Juan')
-let maude = new Traveler('Maude')
-console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 2. The wagon starts with 2 seats. We haven't added travelers to the wagon yet.`)
-wagon.join(henrietta)
-console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 1. Henrietta just joined.`)
-wagon.join(juan)
-wagon.join(maude) // There is no room for her!
-console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 0 – There is no room for Maude, but Juan was able to join.`)
-henrietta.hunt() // Henrietta goes in search of food.
-juan.eat() // Juan eats – as Juan does. 🤣
-juan.eat() // Juan has run out of food!
-console.log(juan)
-console.log(`Wagon Should Quarantine?: ${ wagon.shouldQuarantine() } – EXPECTED: true. Juan has run out of food and become unhealthy!`)
-console.log(`Wagon's Total Food?: ${ wagon.totalFood() } – EXPECTED: 3.`)
+// Create a wagon that can hold 4 people
+let wagon = new Wagon(4);
+// Create five travelers
+let henrietta = new Traveler('Henrietta');
+let juan = new Traveler('Juan');
+let drsmith = new Doctor('Dr. Smith');
+let sarahunter = new Hunter('Sara');
+let maude = new Traveler('Maude');
+console.log(`#1: There should be 4 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+wagon.join(henrietta);
+console.log(`#2: There should be 3 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+wagon.join(juan);
+wagon.join(drsmith);
+wagon.join(sarahunter);
+wagon.join(maude); // There isn't room for her!
+console.log(`#3: There should be 0 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+console.log(`#4: There should be 5 total food. Actual: ${wagon.totalFood()}`);
+sarahunter.hunt(); // gets 5 more food
+drsmith.hunt();
+console.log(`#5: There should be 12 total food. Actual: ${wagon.totalFood()}`);
+henrietta.eat();
+sarahunter.eat();
+drsmith.eat();
+juan.eat();
+juan.eat(); // juan is now hungry (sick)
+console.log(`#6: Quarantine should be true. Actual: ${wagon.shouldQuarantine()}`);
+console.log(`#7: There should be 7 total food. Actual: ${wagon.totalFood()}`);
+drsmith.heal(juan);
+console.log(`#8: Quarantine should be false. Actual: ${wagon.shouldQuarantine()}`);
+sarahunter.giveFood(juan, 4);
+sarahunter.eat(); // She only has 1, so she eats it and is now sick
+console.log(`#9: Quarantine should be true. Actual: ${wagon.shouldQuarantine()}`);
+console.log(`#10: There should be 6 total food. Actual: ${wagon.totalFood()}`);
 
 const main = document.querySelector('main')
 let currentTravelers = document.createElement('div')
 currentTravelers.id = 'wagon1'
 main.append(currentTravelers)
-currentTravelers.append(JSON.stringify(juan),' ', JSON.stringify(maude),' ', JSON.stringify(henrietta))
+currentTravelers.append(JSON.stringify(wagon))
 
 let seats = document.createElement('div')
 seats.id = 'wagonCapacity'
-seats.append('Seats available:',' ', wagon.getAvailableSeatCount())
+seats.append('Seats available:', ' ', wagon.getAvailableSeatCount())
 main.append(seats)
 
 let food = document.createElement('div')
 food.id = 'totalFood'
-food.append('Total food :',' ', wagon.totalFood())
+food.append('Total food :', ' ', wagon.totalFood())
 main.append(food)
 
 let quarentine = document.createElement('div')
 quarentine.id = 'shouldQuarentine'
-quarentine.append('shouldQuarentine:',' ', wagon.shouldQuarantine())
+quarentine.append('shouldQuarentine:', ' ', wagon.shouldQuarantine())
 main.append(quarentine)
 
 
@@ -43,4 +59,4 @@ const hunting = document.querySelector('#hunt')
 const shouldQuarentine = document.querySelector('#shouldQuarentine')
 const addTraveler = document.querySelector('#addTraveler')
 const travelerName = document.querySelector('#travelerName')
-
+const newWagonCapacity = document.querySelector('#newWagonCapacity')
